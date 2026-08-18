adeb5dc4cd205a8b960dd0d9d7ed8d7198e2343e
Thiranjaya Siriwardhana <kanujathiranjaya@gmail.com>
2026-08-18 13:51:36 +0530

    feat: add ESLint configuration for TypeScript and Angular with disabled rule overrides

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -10,35 +10,36 @@ module.exports = defineConfig([
     extends: [
       eslint.configs.recommended,
       tseslint.configs.recommended,
-      tseslint.configs.stylistic,
       angular.configs.tsRecommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@angular-eslint/prefer-inject": "off",
+      "@angular-eslint/directive-selector": "off",
+      "@angular-eslint/component-selector": "off",
+      "@angular-eslint/no-empty-lifecycle-method": "off",
+      "prefer-const": "off",
+      "no-empty": "off",
+      "no-useless-escape": "off",
+      "no-constant-condition": "off",
+      "no-case-declarations": "off",
+      "no-prototype-builtins": "off",
     },
   },
   {
     files: ["**/*.html"],
     extends: [
       angular.configs.templateRecommended,
-      angular.configs.templateAccessibility,
     ],
-    rules: {},
+    rules: {
+      "@angular-eslint/template/prefer-control-flow": "off",
+      "@angular-eslint/template/click-events-have-key-events": "off",
+      "@angular-eslint/template/interactive-supports-focus": "off",
+      "@angular-eslint/template/elements-content": "off",
+      "@angular-eslint/template/label-has-associated-control": "off",
+    },
   }
 ]);
